400a895b242c33fc74e86398f7626514ac125de2
AndrewBalaichuk211 <andrian-maksym.balaichuk-pi1901@kep.nung.edu.ua>
2022-04-17 23:38:57 +0200

    main

--- js/lr5.js
+++ js/lr5.js
@@ -1,17 +1,17 @@
 function insert(num) {
-    var a = document.form.textview;
+    var a = document.querySelector('.textview');
     a.value += num;
 }
 
 function opt(amd) {
-    var view = document.form.textview;
+    var view = document.querySelector('.textview');
     if (view.value != "") {
         view.value += amd;
     }
 }
 
 function zero() {
-    var view = document.form.textview;
+    var view = document.querySelector('.textview');
     if (view.value != "") {
         view.value += '0';
     }
@@ -19,13 +19,13 @@ function zero() {
 
 function equal() {
     var button = document.getElementsByClassName('none');
-    var a = document.form.textview;
+    var a = document.querySelector('.textview');
     var exp = a.value;
 
     if (exp) {
         try { a.value = eval(exp); } catch (e) {
             alert("Syntax Error!");
-            document.form.textview.value = 'Syntax Error!';
+            document.querySelector('.textview').value = 'Syntax Error!';
             none();
         }
     }
@@ -33,7 +33,7 @@ function equal() {
         a.value = "";
     }
     if (a.value == 'Infinity') {
-        document.form.textview.value = "Can't divide by Zero!";
+        document.querySelector('.textview').value = "Can't divide by Zero!";
         // button.onclick ='none()';
         alert('Can\'t divide by Zero!')
         none();
@@ -45,5 +45,5 @@ function none() {
 }
 
 function clean() {
-    document.form.textview.value = '';
+    document.querySelector('.textview').value = '';
 }
